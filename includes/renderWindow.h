eb68bb132f6e4bf59d5baf1615bfba9d25cc3ea0
TANK1_41 <bradenaey1@gmail.com>
2022-04-18 14:11:12 -0400

    window rendering

---
+++ includes/renderWindow.h
@@ -0,0 +1,40 @@
+
+
+#ifndef DATA_STRUCTURES_VISUALIZED_RENDERWINDOW_H
+#define DATA_STRUCTURES_VISUALIZED_RENDERWINDOW_H
+
+
+#include <iostream>
+#include <vector>
+#include "SDL.h"
+
+class renderWindow {
+public:
+  renderWindow();
+
+  virtual ~renderWindow();
+
+  void init(const char *title, int xpos, int ypos, int width, int height,
+            bool fullscreen);
+
+  void handleEvents();
+
+  void update();
+
+  void render();
+
+  bool running();
+
+  void restart();
+
+  SDL_Window *getWindow() const;
+
+  SDL_Renderer *renderer;
+  std::vector<SDL_Event> events;
+
+private:
+  bool isRunning;
+  SDL_Window *window;
+};
+
+#endif // DATA_STRUCTURES_VISUALIZED_RENDERWINDOW_H
